6b02ae3c72845ee304417e88325a29583d43981b
UnderGround54 <martinpros54@gmail.com>
2022-07-29 17:24:45 +0300

    Créer la formulaire d'ajout -> controller d'ajout -> ajout

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,17 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recipe', name: 'recipe.list')]
-    public function index(): Response
+    /**
+     * Cette fonction retourne tous les recettes
+     *
+     * @param RecipeRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/recipe', name: 'recipe.list', methods: ['GET'])]
+    public function index(RecipeRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
-        return $this->render('recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
+        $recipe = $paginator->paginate(
+            $repository->findAll(), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+        return $this->render('pages/recipe/index.html.twig', [
+            'recipes' => $recipe,
         ]);
+    }
+
+    #[Route('/nouveau/recette', name:'recipe.new', methods: ['GET','POST'])]
+    public function new(EntityManagerInterface $manager, Request $request) : Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $recipe = $form->getData();
+            $manager->persist($recipe); //comme commit enregister dans une local storage
+            $manager->flush(); //push les données dans une localstorage
+
+            $this->addFlash(
+                'Succes',
+                'Votre recette à été créer avec succes !'
+            );
+
+            return $this->redirectToRoute('recipe.list'); // ingredient is the name of route / index
+        }
+        return $this->render('pages/recipe/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
     }
 }
---
+++ src/Form/RecipeType.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ingredient;
+use App\Entity\Recipe;
+use App\Repository\IngredientRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class RecipeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' =>'50'
+                ],
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' =>2, 'max' => 50]),
+                    New Assert\NotBlank()
+                ]
+            ])
+            ->add('time', IntegerType::class,[
+                'attr' => [
+                    'class' => 'form-control',
+                    'min' => 1,
+                    'max' => 1040
+                ],
+                'label' => 'Temps (en minutes)',
+                'label_attr' => [
+                    'class' => 'form-label mt-4',
+                ],
+                'constraints' => [
+                    new Assert\Positive(),
+                    New Assert\LessThan(1441)
+                ]
+            ])
+            ->add('nbPersonne',IntegerType::class ,[
+                'attr' => [
+                    'class' => 'form-control',
+                    'min' => 1,
+                    'max' => 50
+                ],
+                'label' => 'Nombre de personnes',
+                'label_attr' => [
+                    'class' => 'form-label mt-4',
+                ],
+                'constraints' => [
+                    new Assert\Positive(),
+                    New Assert\LessThan(51)
+                ]
+            ])
+            ->add('difficulty',RangeType::class,[
+                'attr' => [
+                    'class' => 'form-range',
+                    'min' => 1,
+                    'max' => 5
+                ],
+                'label' => 'Difficulté',
+                'label_attr' => [
+                    'class' => 'form-label mt-4',
+                ],
+                'constraints' => [
+                    new Assert\Positive(),
+                    New Assert\LessThan(1441)
+                ]
+            ])
+            ->add('description', TextareaType::class,[
+                'attr' => [
+                    'class' => 'form-control',
+                    'min' => 5,
+                    'max' => 1000
+                ],
+                'label' => 'Description',
+                'label_attr' => [
+                    'class' => 'form-label mt-4',
+                ],
+                'constraints' => [
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('prix', MoneyType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'label' => 'Prix',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Positive(),
+                    New Assert\LessThan(1001)
+                ]
+            ])
+            ->add('isFavorie', CheckboxType::class,[
+                'attr' => [
+                    'class' => 'form-check'
+                ],
+                'label' => 'Favoris ?',
+                'label_attr' => [
+                    'class' => 'form-label mt-4',
+                ],
+                'constraints' => [
+                    new Assert\NotNull()
+                ]
+            ])
+            ->add('ingredients', EntityType::class, [
+                'class' => Ingredient::class,
+                'query_builder' => function (IngredientRepository $ingredient){
+                    return $ingredient->createQueryBuilder('i')
+                                    ->orderBy('i.nom','ASC');
+                },
+                'choice_label' => 'nom',
+                'multiple' => 'true',
+                'expanded' => 'true',
+                'label' => 'Les ingrédients',
+                'label_attr' => [
+                    'class' => 'form-label mt-4',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4'
+                ],
+                'label' => 'Créer ma recette'
+            ]) // Il faut ajouter submit
+            // ->add('dateCreation')
+            // ->add('dateUpdate') générer automatiquement
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipe::class,
+        ]);
+    }
+}
