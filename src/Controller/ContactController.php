c7d3567c7817d7772f02d14639d562dfeccdb172
UnderGround54 <martinpros54@gmail.com>
2022-08-09 15:59:38 +0300

    creer mailer send email par mailtrap

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,15 +5,26 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
 
 class ContactController extends AbstractController
 {
+    /**
+     * envoyer email
+     * 
+     * @param EntityManagerInterface $manager
+     * @param Request $request
+     * @param MailerInterface $mailer
+     * @return Response
+     */
     #[Route('/contact', name: 'contact')]
-    public function index(EntityManagerInterface $manager, Request $request): Response
+    public function index(EntityManagerInterface $manager, Request $request, MailerInterface $mailer): Response
     {
         $contact = new Contact();
         if($this->getUser()){
@@ -29,6 +40,19 @@ class ContactController extends AbstractController
 
             $manager->persist($contact);
             $manager->flush();
+
+            $email = (new TemplatedEmail())->from($contact->getEmail())
+                        ->to('admin@recette.com')
+                        ->subject($contact->getSubject())
+                        ->htmlTemplate('emails/contact.html.twig')
+                        ->context([
+                            'contact' => $contact,
+                        ]);
+            try {
+                $mailer->send($email);
+            } catch (TransportExceptionInterface $e) {
+                dd($e);
+            }
 
             $this->addFlash(
                 'Succes',
